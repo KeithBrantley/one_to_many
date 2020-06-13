7594fa974d22dd4a87b296535787256636d7e525
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-13 13:49:15 -0700

    Added input to html page

--- one_to_many/main/urls.py
+++ one_to_many/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('process_chef', views.process_chef),
+    path('process_dish', views.process_dish),
 ]
--- one_to_many/main/views.py
+++ one_to_many/main/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Chef, Dish
 
 # Create your views here.
 def index(request):
+    context = {
+        'all_chefs': Chef.objects.all(),
+        'all_dishes': Dish.objects.all(),
+
+    }
     return render(request, 'index.html', context)
+
+def process_chef(request):
+    Chef.objects.create(
+        name=request.POST['name'],
+        cuisine=request.POST['cuisine'],
+        rank=request.POST['rank'],
+    )
+    return redirect('/')
+def process_dish(request):
+    this_chef = Chef.objects.get(id=request.POST["maker"])
+    Dish.objects.create(
+        name=request.POST["name"],
+        tastiness_level=request.POST["tastiness_level"],
+        spice=request.POST["spice"],
+        maker=this_chef
+    )
+    return redirect('/')
